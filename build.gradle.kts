d0ed0b6d97dd006343cc92d6b74d45afaedc6df7
Naulian <cinkhangin@gmail.com>
2024-09-14 19:51:58 +0800

    Add processing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
-    kotlin("jvm") version "2.0.10"
+    java
+    kotlin("jvm") version "2.0.0"
+    `maven-publish`
 }
 
 group = "com.naulian.kproc"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -11,11 +13,27 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation(fileTree("libs") { include("*.jar") })
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(17)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven"){
+                from(components["java"])
+                groupId = "com.naulian"
+                artifactId = "processing"
+                version = "0.0.1-test01"
+            }
+        }
+    }
 }
